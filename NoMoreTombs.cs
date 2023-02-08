4e10ae615b07378f3259fb3f0c5101f0bf5314b5
Sean Smith <appl9000@yahoo.com>
2023-02-08 02:41:10 -0800

    Added craftable gravestones

--- NoMoreTombs.cs
+++ NoMoreTombs.cs
@@ -1,4 +1,10 @@
-﻿using Terraria.ModLoader;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace NoMoreTombs
 {
@@ -8,5 +14,39 @@ namespace NoMoreTombs
 		{
 			TombDetours.ApplyDetours();
 		}
+		public override void AddRecipes()
+		{
+            Recipe gravestone = Recipe.Create(ItemID.Gravestone);
+            gravestone.AddIngredient(ItemID.StoneBlock, 20);
+            gravestone.AddTile(TileID.WorkBenches);
+            gravestone.Register();
+
+            Recipe tombstone = Recipe.Create(ItemID.Tombstone);
+            tombstone.AddIngredient(ItemID.StoneBlock, 20);
+            tombstone.AddTile(TileID.WorkBenches);
+            tombstone.Register();
+
+            Recipe headstone = Recipe.Create(ItemID.Headstone);
+            headstone.AddIngredient(ItemID.StoneBlock, 20);
+            headstone.AddTile(TileID.WorkBenches);
+            headstone.Register();
+
+            Recipe crossgrave = Recipe.Create(ItemID.CrossGraveMarker);
+            crossgrave.AddIngredient(ItemID.StoneBlock, 10);
+            crossgrave.AddIngredient(ItemID.Wood, 10);
+            crossgrave.AddTile(TileID.WorkBenches);
+            crossgrave.Register();
+
+            Recipe gravemarker = Recipe.Create(ItemID.GraveMarker);
+            gravemarker.AddIngredient(ItemID.StoneBlock, 10);
+            gravemarker.AddIngredient(ItemID.Wood, 10);
+            gravemarker.AddTile(TileID.WorkBenches);
+            gravemarker.Register();
+
+            Recipe obelisk = Recipe.Create(ItemID.Obelisk);
+            obelisk.AddIngredient(ItemID.Obsidian, 20);
+            obelisk.AddTile(TileID.WorkBenches);
+            obelisk.Register();
+        }
 	}
 }
